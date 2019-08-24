cec4f494eb7a41b2fab966a7b7bf09a2445ba93b
EricBollar <esbollar@gmail.com>
2019-08-24 19:16:56 -0400

    finished SpringTime HW 2 assignment

--- Adventures_of_Karel_2019/SpringTime.java
+++ Adventures_of_Karel_2019/SpringTime.java
@@ -1,11 +1,68 @@
 import stanford.karel.*;
 
+// Eric Bollar
+// AP CS Period A
+// August 24, 2019
+
 public class SpringTime extends Karel
 {
-
+    
     public void run()
     {
-
+        for (int treesLeft = 5; treesLeft > 0; treesLeft--) {
+            findStump();
+            climbTree();
+            makeTree();
+            returnToGround();
+        }
+    }
+    
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    
+    public void findStump() {
+        while (frontIsClear()) {
+            move();
+        }
+    }
+    
+    public void climbTree() {
+        while (frontIsBlocked()) {
+            ascend();
+        }
+    }
+    
+    public void ascend() {
+        turnLeft();
+        move();
+        turnRight();
+    };
+    
+    public void descend() {
+        turnRight();
+        move();
+        turnLeft();
+    }
+    
+    public void makeTree() {
+        putBeeper();
+        ascend();
+        putBeeper();
+        move();
+        putBeeper();
+        descend();
+        putBeeper();
+    }
+    
+    public void returnToGround() {
+        turnRight(); // facing down
+        while (frontIsClear()) {
+            move();
+        }
+        turnLeft();
     }
 
 }

36fa4a04fce036a71fffc4aff37075bbc2c7a506
EricBollar <esbollar@gmail.com>
2019-09-01 16:53:07 -0700

    made StopLight

--- Java_Day_2/Stoplight.java
+++ Java_Day_2/Stoplight.java
@@ -4,6 +4,10 @@ import acm.graphics.*;
 import java.awt.Color;
 import java.awt.event.*;
 
+// Eric Bollar
+// AP CS
+// September 1, 2019
+
 public class Stoplight extends GraphicsProgram
 {
     private static final double BULB_RADIUS = 60; 
@@ -12,8 +16,42 @@ public class Stoplight extends GraphicsProgram
 
     public void run()
     {
-        
+        makeBorder();
+        makeRed();
+        makeYellow();
+        makeGreen();
     }
-
+    
+    void makeBorder() {
+        GRect border = new GRect(WINDOW_WIDTH/2 - BULB_RADIUS, WINDOW_HEIGHT/2 - BULB_RADIUS * 3, 
+                                 BULB_RADIUS * 2, BULB_RADIUS * 6);
+        border.setFilled(true);
+        border.setColor(Color.gray);
+        add(border);
+    }
+    
+    void makeRed() {
+        GOval redCircle = new GOval(WINDOW_WIDTH/2 - BULB_RADIUS, WINDOW_HEIGHT/2 - BULB_RADIUS * 3,
+                                    BULB_RADIUS * 2, BULB_RADIUS * 2);
+        redCircle.setFilled(true);
+        redCircle.setColor(Color.red);
+        add(redCircle);
+    }
+    
+    void makeYellow() {
+        GOval yellowCircle = new GOval(WINDOW_WIDTH/2 - BULB_RADIUS, WINDOW_HEIGHT/2 - BULB_RADIUS,
+                                    BULB_RADIUS * 2, BULB_RADIUS * 2);
+        yellowCircle.setFilled(true);
+        yellowCircle.setColor(Color.yellow);
+        add(yellowCircle);
+    }
+    
+    void makeGreen() {
+        GOval greenCircle = new GOval(WINDOW_WIDTH/2 - BULB_RADIUS, WINDOW_HEIGHT/2 + BULB_RADIUS,
+                                    BULB_RADIUS * 2, BULB_RADIUS * 2);
+        greenCircle.setFilled(true);
+        greenCircle.setColor(Color.green);
+        add(greenCircle);
+    }
 
 }
